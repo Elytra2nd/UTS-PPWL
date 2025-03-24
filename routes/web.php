9c2ed2389044ae5e6c025e68da41329dfe87512e
InnaHenita <innahenitacahyanurani@gmail.com>
2025-03-24 19:19:17 +0700

    Resolve merge conflict in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ObatController;
+use App\Http\Controllers\CartController;
 Route::get('/', function () {
     return view('welcome');
 });
 // routes/web.php
-use App\Http\Controllers\ObatController;
+
 Route::get('/obat', [ObatController::class, 'index'])->name('obat.index');
 Route::get('/obat/cari', [ObatController::class, 'search'])->name('obat.search');
 
-use App\Http\Controllers\CartController;
+
 Route::middleware('auth')->group(function () {
     Route::get('/cart', [CartController::class, 'index']);
     Route::post('/cart', [CartController::class, 'store']);
     
 });
-
-require __DIR__.'/auth.php';
 
